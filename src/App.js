0bd5e6b7d59e5f5c2c8219b417cc867df686b886
toshko66 <toshko66@abv.bg>
2023-09-19 16:06:54 +0300

    checkout added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Authentication from './routes/authentication/authentication.component.jsx';
 import Home from './routes/home/home.component.jsx';
+import Checkout from './routes/checkout/checkout.component.jsx';
 import Navigation from './routes/navigation/navigation.component.jsx';
 import { Routes, Route } from 'react-router-dom';
 import Shop from './routes/shop/shop.component.jsx';
@@ -12,6 +13,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout /> } />
       </Route>
 
     </Routes>
